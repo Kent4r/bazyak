2d07944caa800e87892fccfe7076459d60caf5ce
Kent4r <53953044+Kent4r@users.noreply.github.com>
2024-10-23 19:25:33 +0300

    qwewqe

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import cianparser
 from time import sleep
-a=0
+a=2
 while a < 54:
     data = cianparser.CianParser(location="Москва").get_flats(deal_type="sale",
-                                                              rooms=3,
+                                                              rooms=4,
                                                               with_saving_csv=True,
                                                               with_extra_data=True,
                                                               additional_settings={"start_page":1+a, "end_page":2+a})
     print("----------------------------------------------------")
-    print("Waiting 30 sec")
-    sleep(30)
+    print("Waiting 120 sec")
+    sleep(120)
     a+=2
