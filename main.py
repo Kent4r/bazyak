4130de7d05bc5ba964036174798df3d31b16d524
Kent4r <53953044+Kent4r@users.noreply.github.com>
2024-10-24 22:38:34 +0300

    klkl

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from time import sleep
 a=2
 while a < 54:
     data = cianparser.CianParser(location="Московская Область").get_flats(deal_type="sale",
-                                                              rooms=4,
+                                                              rooms=3,
                                                               with_saving_csv=True,
                                                               with_extra_data=True,
                                                               additional_settings={"start_page":1+a, "end_page":2+a})
