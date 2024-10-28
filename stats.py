630e478021b1f74f79e16e5520fbeb8eb7a1c785
Kent4r <53953044+Kent4r@users.noreply.github.com>
2024-10-28 23:00:47 +0300

    Голицыно куда полетело эй

--- stats.py
+++ stats.py
@@ -2,7 +2,4 @@ import pandas as pd
 
 df = pd.read_csv('cleaned_11K.csv', index_col=0)
 
-del df['deal_type']
-del df['accommodation_type']
-
-df.to_csv('cleaned_11K.csv', index=False)
+print(df['location'].value_counts())
