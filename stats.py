f47df799c3c48c88e91d73e05598608c6b0ffaa0
Kent4r <53953044+Kent4r@users.noreply.github.com>
2024-10-30 00:32:46 +0300

    рпаовпааыи

--- stats.py
+++ stats.py
@@ -4,11 +4,19 @@ import seaborn as sns
 import numpy as np
 
 df = pd.read_csv('cleaned_11K.csv', index_col=0)
+list = ["price","year_of_construction","floor","floors_count","rooms_count","phone"]
+for obj in list:
+    df[obj] = df[obj].fillna(0)
+    df[obj] = df[obj].astype(int)
 
-# year_mode_value = df['year_of_construction'].mode()[0]
-# df['year_of_construction'] = df['year_of_construction'].fillna(year_mode_value)
-# df['year_of_construction'] = df['year_of_construction'].astype(int)
-# df['year_of_construction'].value_counts().to_csv('years_count1.csv')
+print(df.dtypes)
+df.drop(['url', 'phone'], axis=1, inplace=True)
+df.to_csv('truly_cleaned_11K.csv')
+
+year_mode_value = df['year_of_construction'].mode()[0]
+df['year_of_construction'] = df['year_of_construction'].fillna(year_mode_value)
+df['year_of_construction'] = df['year_of_construction'].astype(int)
+df['year_of_construction'].value_counts().to_csv('years_count.csv')
 
 
 
