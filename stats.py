487edaf66c252bd111c5a908464adf78ce2cfec4
Kent4r <egor.bukharov06@gmail.com>
2024-10-29 12:54:30 +0300

    hjdsfkjdv

--- stats.py
+++ stats.py
@@ -1,5 +1,16 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-df = pd.read_csv('11K.csv', index_col=0)
+# df = pd.read_csv('cleaned_11K.csv', index_col=0)
 
-print(df['house_material_type'].value_counts())
+# print(df['year_of_construction'].value_counts())
+# df['year_of_construction'].value_counts().to_csv('years_count.csv')
+
+dash = pd.read_csv('years_count.csv')
+
+plot = sns.barplot(x='year_of_construction', y='count', data=dash, palette='dark:pink', orient='v')
+plot.set_xticklabels(plot.get_xticklabels(), rotation=90, ha='right')
+sns.despine(left=True, bottom=True)
+
+plt.show()
