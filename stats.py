6ca010845700b66869cc2bc466f983084ff2bd6c
Kent4r <egor.bukharov06@gmail.com>
2024-10-29 11:22:32 +0300

    kufiuhtrf

--- stats.py
+++ stats.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-df = pd.read_csv('cleaned_11K.csv', index_col=0)
+df = pd.read_csv('11K.csv', index_col=0)
 
-print(df['location'].value_counts())
+print(df['house_material_type'].value_counts())
