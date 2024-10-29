1448f154f8fbdf2179338b118eb693a42c01c87d
Kent4r <53953044+Kent4r@users.noreply.github.com>
2024-10-30 01:21:06 +0300

    АААААААААААААААААААААААААААА БОЛЬ

--- stats.py
+++ stats.py
@@ -4,19 +4,18 @@ import seaborn as sns
 import numpy as np
 
 df = pd.read_csv('cleaned_11K.csv', index_col=0)
-list = ["price","year_of_construction","floor","floors_count","rooms_count","phone"]
+list = ["price","year_of_construction","floor","floors_count","rooms_count"]
 for obj in list:
     df[obj] = df[obj].fillna(0)
     df[obj] = df[obj].astype(int)
 
 print(df.dtypes)
-df.drop(['url', 'phone'], axis=1, inplace=True)
-df.to_csv('truly_cleaned_11K.csv')
+# del df['phone']
+df.to_csv('cleaned_11K.csv')
 
-year_mode_value = df['year_of_construction'].mode()[0]
-df['year_of_construction'] = df['year_of_construction'].fillna(year_mode_value)
-df['year_of_construction'] = df['year_of_construction'].astype(int)
-df['year_of_construction'].value_counts().to_csv('years_count.csv')
+# df['year_of_construction'] = df['year_of_construction'].fillna(-1)
+# df['year_of_construction'] = df['year_of_construction'].astype(int)
+# df['year_of_construction'].value_counts().to_csv('years_count.csv')
 
 
 
